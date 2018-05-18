1a92804b548d105b66a289eed3a3db5126ef3bf0
soumyapatro <soumyachaitanya4315@gmail.com>
2018-05-18 10:17:21 -0700

    Changing convnet parameters

--- cnn_models/alexnet.py
+++ cnn_models/alexnet.py
@@ -9,7 +9,7 @@ from keras.applications.imagenet_utils import preprocess_input
 from IPython.display import SVG
 from keras.utils.vis_utils import model_to_dot
 from keras.utils import plot_model
-from keras.initializers import glorot_uniform
+from keras.initializers import glorot_normal
 import scipy.misc
 
 import keras.backend as K
@@ -27,33 +27,40 @@ def alexnet(input_shape = (256,256,3), labels = 2):
     I_input = Input(input_shape)
     
     #Stage 1
-    I = Conv2D(filters=96, kernel_size=(11,11), strides=(4,4), padding='valid', activation='relu', use_bias=True,
-               kernel_initializer='glorot_uniform', bias_initializer='zeros', name = 'conv1' )(I_input)
+    I = Conv2D(filters=96, kernel_size=(11,11), strides=(4,4), padding='valid', use_bias=True,
+               kernel_initializer=glorot_normal(seed=1), bias_initializer='zeros', name = 'conv1' )(I_input)
+    I = Activation('relu')(I)
     I = MaxPooling2D((3, 3), strides=(2, 2))(I)
     
     #Stage 2
-    I = Conv2D(filters=256, kernel_size=(5,5), strides=(1,1), padding='same', activation='relu', use_bias=True,
-               kernel_initializer='glorot_uniform', bias_initializer='zeros', name = 'conv2' )(I)
+    I = Conv2D(filters=256, kernel_size=(5,5), strides=(1,1), padding='same', use_bias=True,
+               kernel_initializer=glorot_normal(seed=1), bias_initializer='zeros', name = 'conv2' )(I)
+    I = Activation('relu')(I)
     I = MaxPooling2D((3, 3), strides=(2, 2))(I)
     
     #Stage 3
-    I = Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), padding='same', activation='relu', use_bias=True,
-               kernel_initializer='glorot_uniform', bias_initializer='zeros', name = 'conv3' )(I)
-
+    I = Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), padding='same', use_bias=True,
+               kernel_initializer=glorot_normal(seed=1), bias_initializer='zeros', name = 'conv3' )(I)
+    I = Activation('relu')(I)
+    
     #Stage 4
-    I = Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), padding='same', activation='relu', use_bias=True,
-               kernel_initializer='glorot_uniform', bias_initializer='zeros', name = 'conv4' )(I)
+    I = Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), padding='same', use_bias=True,
+               kernel_initializer=glorot_normal(seed=1), bias_initializer='zeros', name = 'conv4' )(I)
+    I = Activation('relu')(I)
     
     #Stage 5
-    I = Conv2D(filters=256, kernel_size=(3,3), strides=(1,1), padding='same', activation='relu', use_bias=True,
-               kernel_initializer='glorot_uniform', bias_initializer='zeros', name = 'conv5' )(I)
+    I = Conv2D(filters=256, kernel_size=(3,3), strides=(1,1), padding='same', use_bias=True,
+               kernel_initializer=glorot_normal(seed=1), bias_initializer='zeros', name = 'conv5' )(I)
+    I = Activation('relu')(I)
     I = MaxPooling2D((3, 3), strides=(2, 2))(I)
+    
     
     #Flatten & Fully Connected layers
     I = Flatten()(I)
-    I = Dense(units=4096, activation='relu', name='fc1', kernel_initializer = 'glorot_uniform')(I)
-    I = Dense(units=4096, activation='relu', name='fc2', kernel_initializer = 'glorot_uniform')(I)
-    I = Dense(labels, activation='relu', name='fc3' + str(labels), kernel_initializer = 'glorot_uniform')(I)
+    I = Dense(units=4096, name='fc1', activation= 'relu', kernel_initializer = glorot_normal(seed=1))(I)
+    I = Dense(units=4096, name='fc2', activation= 'relu', kernel_initializer = glorot_normal(seed=1))(I)
+    I = Dense(labels, name='fc3' + str(labels), kernel_initializer = glorot_normal(seed=1))(I)
+    I = Activation('sigmoid')(I)
     
     model = Model(inputs = I_input, outputs = I, name='AlexNet')
     
